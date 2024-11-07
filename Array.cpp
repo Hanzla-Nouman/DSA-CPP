0ee0b941e4b64ea995529a54529e1bb490748ec4
Hanzla-Nouman <hanzlanouman514@gmail.com>
2024-11-08 04:00:52 +0500

    Arrays

---
+++ Array.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+void changeArr(int arr[],int size){
+    cout<<"In function"<<endl;
+      for (int i = 0; i < size; i++)
+      {
+          arr[i] =  arr[i]*2;
+      }
+      
+};
+
+int findTarget(int arr[],int size,int target){  // Linear Serach
+     for (int i = 0; i < size; i++)
+     {
+        if (arr[i] == target)
+        {
+           return i;
+        }
+        
+     }
+     return -1;
+}
+
+int main()
+{
+      int marks[5] = {67,33,90,89,95};
+    //   int marks2[] = {67,33,90,89,95};
+      marks[1] = 46;
+    //   cout<<marks[2]<<endl;
+    //   cout<<marks[1]<<endl;
+
+int size = 5;
+   int marks3[size];
+//    cout<<"Input Numbers: \n";
+//     for (int i = 0; i < size ; i++)
+//     {
+//         cin>>marks[i];
+//     }
+//     int largest = INT32_MIN;
+//     int smallest = __INT_MAX__;
+//     int sindex;
+//     int lindex;
+
+//     for (int i = 0; i < size ; i++)
+//     {
+//         if(marks[i] > largest){
+//             largest = marks[i];
+//             lindex = i;
+//         }
+//         if(marks[i] < smallest){
+//             sindex = i;
+//             smallest = marks[i];
+//         }
+
+//         // smallest = min(marks[i],smallest);
+//         // largest = max(marks[i],smallest);
+//     }
+//     cout<<"Largest Number: "<<largest <<endl;
+//     cout<<"Smallest Number: "<<smallest <<endl;
+//     cout<<"Largest Index: "<<lindex <<endl;
+//     cout<<"Smallest Index: "<<sindex <<endl;
+
+int nums[] ={12,14,16,18};   // Pass by reference
+ changeArr(nums,4);
+ cout<<"In main"<<endl;
+ for (int i = 0; i < 4; i++)
+ {
+   cout<< nums[i]<<endl;
+ }
+ 
+cout<< findTarget(nums,4,90);
+    return 0;
+}
--- pattern.cpp
+++ pattern.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-                     // i = 0     , j= 1
-                     // i = 1     , j= 2
-                     // i = 2     , j= 3
-                     // i = 3     , j= 4
-                     // i = 4     , j= 5
+                     // i = 0     ,   j= 1
+                     // i = 1     ,   j= 2
+                     // i = 2     ,   j= 3
+                     // i = 3     ,   j= 4
+                     // i = 4     ,   j= 5
 
   int n =10;
     for (int i = 0; i < n; i++)
